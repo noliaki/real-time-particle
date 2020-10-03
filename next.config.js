0a7a0606fd38a452f2de52cb6b552ff1093ef3a9
noliaki <noliaki@gmail.com>
2020-10-04 01:24:20 +0900

    wip

--- next.config.js
+++ next.config.js
@@ -12,14 +12,40 @@ module.exports = {
       ],
     })
 
-    config.optimization.minimize = isProd
-    config.optimization.minimizer.push(new TerserPlugin({
-      terserOptions: {
-        compress: {
-          drop_console: process.env.NODE_ENV !== 'development'
-        }
+    if (!config.optimization) {
+      config.optimization = {
+        minimize: isProd,
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              compress: {
+                drop_console: process.env.NODE_ENV !== 'development'
+              }
+            }
+          })
+        ]
       }
-    }))
+    } else {
+      config.optimization.minimize = isProd
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: process.env.NODE_ENV !== 'development'
+            }
+          }
+        })
+      ]
+    }
+
+    // config.optimization.minimize = isProd
+    // config.optimization.minimizer.push(new TerserPlugin({
+    //   terserOptions: {
+    //     compress: {
+    //       drop_console: process.env.NODE_ENV !== 'development'
+    //     }
+    //   }
+    // }))
 
     return config
   },
